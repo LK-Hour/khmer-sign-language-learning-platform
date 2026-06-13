3f9d6585c235c03579a63de108aeae59f0152e7f
LK-Hour <l.kimhour.609@gmail.com>
2026-06-13 15:35:58 +0700

    Undo testing

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,4 +1,4 @@
 export { default as AppLayout } from "./AppLayout";
-export { default as MainHeader } from "../header-nav";
+export { default as MainHeader } from "./header-nav";
 export { default as PageContainer } from "./PageContainer";
 export { default as ResponsiveHidden } from "./ResponsiveHidden";
